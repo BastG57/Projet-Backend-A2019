eff66ccb27ebb5a80ff86c6df12dc72b0ecefa91
BastG57 <bastieng57@gmail.com>
2019-09-29 11:04:21 +0200

    tegh

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_27_105214) do
-
-  create_table "students", force: :cascade do |t|
-    t.string "name"
-    t.string "last_name"
-    t.date "birthday"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
