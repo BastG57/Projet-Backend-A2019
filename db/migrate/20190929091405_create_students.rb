ca694de4e81abd560e0475ef31c14c7f7328423c
BastG57 <bastieng57@gmail.com>
2019-09-29 11:17:06 +0200

    fs

--- db/migrate/20190929091405_create_students.rb
+++ db/migrate/20190929091405_create_students.rb
@@ -1,8 +1,8 @@
 class CreateStudents < ActiveRecord::Migration[5.2]
   def change
     create_table :students do |t|
-      t.string :name
-      t.string :last_name
+      t.string :Name
+      t.string :Last_name
       t.date :birthday
 
       t.timestamps
