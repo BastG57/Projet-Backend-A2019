641a45ffc6ba486aeebf8734f8ce402274f5f90a
BastG57 <bastieng57@gmail.com>
2019-10-04 11:44:22 +0200

    CountersIncrement

--- app/models/counter.rb
+++ app/models/counter.rb
@@ -1,2 +1,10 @@
 class Counter < ApplicationRecord
+    test "should have the necessary required validators" do
+        counter = counter.new
+        counter.value = 0
+
+        counter.increment!
+
+        assert counter.value == 1
+    end
 end
