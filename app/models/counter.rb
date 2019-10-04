d2d7120c855e45c32b0b69c30e7998066614238a
BastG57 <bastieng57@gmail.com>
2019-10-04 12:09:09 +0200

    CounterWorking

--- app/models/counter.rb
+++ app/models/counter.rb
@@ -1,10 +1,5 @@
 class Counter < ApplicationRecord
-    test "should have the necessary required validators" do
-        counter = counter.new
-        counter.value = 0
-
-        counter.increment!
-
-        assert counter.value == 1
+    def increment!
+        self.value += 1
     end
 end
